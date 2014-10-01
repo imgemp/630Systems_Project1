400b347155ea0043dcfd272ca47052b0fd0b3e31
Ian Gemp <imgemp@gmail.com>
2014-09-30 22:40:26 -0400

    Fixed callback function
    You should now be able to run in the command line
    > node MasterFile.js
    Still not sure what to do with .ts though since it doesn’t seem to
    recognize node commands (‘require’) during compile

--- MasterFile.js
+++ MasterFile.js
@@ -1,31 +1,48 @@
 // Start Node
-
 var fs = require('fs');
-var bytecode;
-fs.readFile('foo.pyc', function (err, data) {
-	if (err) throw err;
-	bytecode = data;
+fs.readFile('foo.pyc', function doneReading(err, bytecode) {
+    if (err)
+        throw err;
+    interpretBytecode(bytecode);
 });
-var magic_number = bytecode.slice(0,4); //magic number
-var time_stamp = bytecode.slice(4,8); //time stamp
-//check magic number against python 2.7 (03f3 0d0a)
-var my_version = [3,243,13,10];
-// var my_version:Array<number> = [3,243,13,10];
-for (var i=0; i++; i<4) {
-	if (magic_number.readUInt8(i) != my_version[i]) {
-		console.log("Not right version!");
-		// exit program
-	}
+
+function interpretBytecode(bytecode) {
+    // Parse Bytecode and Return Op Codes
+    parseBytecode(bytecode);
+    // Execute Op Codes
 }
-i = 0;
-while (String.fromCharCode(bytecode.slice(8+4*i,12+4*i).readUInt8(0)) != 'c') {
-	i = i + 1;
-	if (i>100) {
-		return;
-	}
+
+function parseBytecode(bytecode) {
+    var magic_number = bytecode.slice(0, 4);
+    var time_stamp = bytecode.slice(4, 8);
+
+    // Check Magic Number Against Python 2.7 (03f3 0d0a)
+    var my_version = [3, 243, 13, 10];
+    for (var i = 0; i++; i < 4) {
+        if (magic_number.readUInt8(i) != my_version[i]) {
+            console.log("Not right version!");
+            // exit program
+        }
+    }
+
+    // Find First Code Object - "c"
+    i = 0;
+    while (String.fromCharCode(bytecode.slice(8 + 4 * i, 12 + 4 * i).readUInt8(0)) != 'c') {
+        i = i + 1;
+        if (i > 100) {
+            return;
+        }
+    }
+    i = 8 + 4 * i + 1;
+
+    // Parse Code Object Fields
+    var argcount = bytecode.slice(i, i + 4).readUInt32LE(0);
+    var nlocals = bytecode.slice(i + 4, i + 8).readUInt32LE(0);
+    var stacksize = bytecode.slice(i + 8, i + 12).readUInt32LE(0);
+    var flags = bytecode.slice(i + 12, i + 16).readUInt32LE(0);
+
+    console.log(argcount);
+    console.log(nlocals);
+    console.log(stacksize);
+    console.log(flags);
 }
-i = 8+4*i;
-var argcount = bytecode.slice(i,i+4).readUInt32LE(0);
-var nlocals = bytecode.slice(i+4,i+8).readUInt32LE(0);
-var stacksize = bytecode.slice(i+8,i+12).readUInt32LE(0);
-var flags = bytecode.slice(i+12,i+16).readUInt32LE(0);
