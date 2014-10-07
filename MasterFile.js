2f57a0046b41b42eab6a66fa56d27dda85b24c28
Shaylyn Adams <sadams@umass.edu>
2014-10-07 11:37:17 -0400

    Updated Version-Fixed Enums

--- MasterFile.js
+++ MasterFile.js
@@ -682,20 +682,26 @@ function interpretBytecode(bytecode) {
     // http://daeken.com/2010-02-20_Python_Marshal_Format.html
     // http://nedbatchelder.com/blog/200804/the_structure_of_pyc_files.html
     parseBytecode(bytecode);
-
-    // console.log(byteObject);
-    // console.log(byteObject.code_object.code);
-    // console.log(byteObject.code_object.code[0]);
-    // console.log(byteObject.code_object.consts[0]);
     console.log(byteObject.interned_list);
-    // Execute Op Codes
+
+    //execute the initial opcodes in the code object
+    execBytecode();
+}
+
+//initalize the stack object
+var Stack = [];
+function execBytecode() {
+    for (var i = 0; i < byteObject.code_object.code.length; i++) {
+        var opcode = byteObject.code_object.code[i][0];
+        var operand = byteObject.code_object.code[i][1];
+        OpCodeFunctions[enums.OpCodeList[opcode](operand)];
+    }
+
+    return Stack.pop();
 }
 
 //initalize object to store information of various types
 var byteObject = {};
-
-//initalize the stack object
-var Stack = [];
 
 var readByType = {};
 readByType['0'] = readNull;
@@ -723,135 +729,140 @@ readByType['c'] = readCodeObject;
 
 //https://android.googlesource.com/platform/prebuilts/python/darwin-x86/2.7.5/+/master/include/python2.7/opcode.h
 //enum list of all opcodes
-var OpCodeList = new Enum({
-    STOP_CODE: 0,
-    POP_TOP: 1,
-    ROT_TWO: 2,
-    ROT_THREE: 3,
-    DUP_TOP: 4,
-    ROT_FOUR: 5,
-    NOP: 9,
-    UNARY_POSITIVE: 10,
-    UNARY_NEGATIVE: 11,
-    UNARY_NOT: 12,
-    UNARY_CONVERT: 13,
-    UNARY_INVERT: 15,
-    BINARY_POWER: 19,
-    BINARY_MULTIPLY: 20,
-    BINARY_DIVIDE: 21,
-    BINARY_MODULO: 22,
-    BINARY_ADD: 23,
-    BINARY_SUBTRACT: 24,
-    BINARY_SUBSCR: 25,
-    BINARY_FLOOR_DIVIDE: 26,
-    BINARY_TRUE_DIVIDE: 27,
-    INPLACE_FLOOR_DIVIDE: 28,
-    INPLACE_TRUE_DIVIDE: 29,
-    SLICE: 30,
-    //SLICE:31,
-    //SLICE:32,
-    //SLICE:33,
-    STORE_SLICE: 40,
-    //STORE_SLICE:41,
-    //STORE_SLICE:42,
-    // STORE_SLICE:43,
-    DELETE_SLICE: 50,
-    // DELETE_SLICE:51,
-    //DELETE_SLICE:52,
-    //DELETE_SLICE:53,
-    STORE_MAP: 54,
-    INPLACE_ADD: 55,
-    INPLACE_SUBTRACT: 56,
-    INPLACE_MULTIPY: 57,
-    INPLACE_DIVIDE: 58,
-    INPLACE_MODULO: 59,
-    STORE_SUBSCR: 60,
-    DELETE_SUBSCR: 61,
-    BINARY_LSHIFT: 62,
-    BINARY_RSHIFT: 63,
-    BINARY_AND: 64,
-    BINARY_XOR: 65,
-    BINARY_OR: 66,
-    INPLACE_POWER: 67,
-    GET_ITER: 68,
-    PRINT_EXPR: 70,
-    PRINT_ITEM: 71,
-    PRINT_NEWLINE: 72,
-    RINT_ITEM_TO: 73,
-    PRINT_NEWLINE_TO: 74,
-    INPLACE_LSHIFT: 75,
-    INPLACE_RSHIFT: 76,
-    INPLACE_AND: 77,
-    INPLACE_XOR: 78,
-    INPLACE_OR: 79,
-    BREAK_LOOP: 80,
-    WITH_CLEANUP: 81,
-    LOAD_LOCALS: 82,
-    RETURN_VALUE: 83,
-    IMPORT_STAR: 84,
-    EXEC_STMT: 85,
-    YIELD_VALUE: 86,
-    POP_BLOCK: 87,
-    END_FINALLY: 88,
-    BUILD_CLASS: 89,
+var OpCodeList;
+(function (OpCodeList) {
+    OpCodeList[OpCodeList["STOP_CODE"] = 0] = "STOP_CODE";
+    OpCodeList[OpCodeList["POP_TOP"] = 1] = "POP_TOP";
+    OpCodeList[OpCodeList["ROT_TWO"] = 2] = "ROT_TWO";
+    OpCodeList[OpCodeList["ROT_THREE"] = 3] = "ROT_THREE";
+    OpCodeList[OpCodeList["DUP_TOP"] = 4] = "DUP_TOP";
+    OpCodeList[OpCodeList["ROT_FOUR"] = 5] = "ROT_FOUR";
+    OpCodeList[OpCodeList["NOP"] = 9] = "NOP";
+    OpCodeList[OpCodeList["UNARY_POSITIVE"] = 10] = "UNARY_POSITIVE";
+    OpCodeList[OpCodeList["UNARY_NEGATIVE"] = 11] = "UNARY_NEGATIVE";
+    OpCodeList[OpCodeList["UNARY_NOT"] = 12] = "UNARY_NOT";
+    OpCodeList[OpCodeList["UNARY_CONVERT"] = 13] = "UNARY_CONVERT";
+    OpCodeList[OpCodeList["UNARY_INVERT"] = 15] = "UNARY_INVERT";
+    OpCodeList[OpCodeList["BINARY_POWER"] = 19] = "BINARY_POWER";
+    OpCodeList[OpCodeList["BINARY_MULTIPLY"] = 20] = "BINARY_MULTIPLY";
+    OpCodeList[OpCodeList["BINARY_DIVIDE"] = 21] = "BINARY_DIVIDE";
+    OpCodeList[OpCodeList["BINARY_MODULO"] = 22] = "BINARY_MODULO";
+    OpCodeList[OpCodeList["BINARY_ADD"] = 23] = "BINARY_ADD";
+    OpCodeList[OpCodeList["BINARY_SUBTRACT"] = 24] = "BINARY_SUBTRACT";
+    OpCodeList[OpCodeList["BINARY_SUBSCR"] = 25] = "BINARY_SUBSCR";
+    OpCodeList[OpCodeList["BINARY_FLOOR_DIVIDE"] = 26] = "BINARY_FLOOR_DIVIDE";
+    OpCodeList[OpCodeList["BINARY_TRUE_DIVIDE"] = 27] = "BINARY_TRUE_DIVIDE";
+    OpCodeList[OpCodeList["INPLACE_FLOOR_DIVIDE"] = 28] = "INPLACE_FLOOR_DIVIDE";
+    OpCodeList[OpCodeList["INPLACE_TRUE_DIVIDE"] = 29] = "INPLACE_TRUE_DIVIDE";
+    OpCodeList[OpCodeList["SLICE_0"] = 30] = "SLICE_0";
+    OpCodeList[OpCodeList["SLICE_1"] = 31] = "SLICE_1";
+    OpCodeList[OpCodeList["SLICE_2"] = 32] = "SLICE_2";
+    OpCodeList[OpCodeList["SLICE_3"] = 33] = "SLICE_3";
+    OpCodeList[OpCodeList["STORE_SLICE_0"] = 40] = "STORE_SLICE_0";
+    OpCodeList[OpCodeList["STORE_SLICE_1"] = 41] = "STORE_SLICE_1";
+    OpCodeList[OpCodeList["STORE_SLICE_2"] = 42] = "STORE_SLICE_2";
+    OpCodeList[OpCodeList["STORE_SLICE_3"] = 43] = "STORE_SLICE_3";
+    OpCodeList[OpCodeList["DELETE_SLICE_0"] = 50] = "DELETE_SLICE_0";
+    OpCodeList[OpCodeList["DELETE_SLICE_1"] = 51] = "DELETE_SLICE_1";
+    OpCodeList[OpCodeList["DELETE_SLICE_2"] = 52] = "DELETE_SLICE_2";
+    OpCodeList[OpCodeList["DELETE_SLICE_3"] = 53] = "DELETE_SLICE_3";
+    OpCodeList[OpCodeList["STORE_MAP"] = 54] = "STORE_MAP";
+    OpCodeList[OpCodeList["INPLACE_ADD"] = 55] = "INPLACE_ADD";
+    OpCodeList[OpCodeList["INPLACE_SUBTRACT"] = 56] = "INPLACE_SUBTRACT";
+    OpCodeList[OpCodeList["INPLACE_MULTIPY"] = 57] = "INPLACE_MULTIPY";
+    OpCodeList[OpCodeList["INPLACE_DIVIDE"] = 58] = "INPLACE_DIVIDE";
+    OpCodeList[OpCodeList["INPLACE_MODULO"] = 59] = "INPLACE_MODULO";
+    OpCodeList[OpCodeList["STORE_SUBSCR"] = 60] = "STORE_SUBSCR";
+    OpCodeList[OpCodeList["DELETE_SUBSCR"] = 61] = "DELETE_SUBSCR";
+    OpCodeList[OpCodeList["BINARY_LSHIFT"] = 62] = "BINARY_LSHIFT";
+    OpCodeList[OpCodeList["BINARY_RSHIFT"] = 63] = "BINARY_RSHIFT";
+    OpCodeList[OpCodeList["BINARY_AND"] = 64] = "BINARY_AND";
+    OpCodeList[OpCodeList["BINARY_XOR"] = 65] = "BINARY_XOR";
+    OpCodeList[OpCodeList["BINARY_OR"] = 66] = "BINARY_OR";
+    OpCodeList[OpCodeList["INPLACE_POWER"] = 67] = "INPLACE_POWER";
+    OpCodeList[OpCodeList["GET_ITER"] = 68] = "GET_ITER";
+    OpCodeList[OpCodeList["PRINT_EXPR"] = 70] = "PRINT_EXPR";
+    OpCodeList[OpCodeList["PRINT_ITEM"] = 71] = "PRINT_ITEM";
+    OpCodeList[OpCodeList["PRINT_NEWLINE"] = 72] = "PRINT_NEWLINE";
+    OpCodeList[OpCodeList["RINT_ITEM_TO"] = 73] = "RINT_ITEM_TO";
+    OpCodeList[OpCodeList["PRINT_NEWLINE_TO"] = 74] = "PRINT_NEWLINE_TO";
+    OpCodeList[OpCodeList["INPLACE_LSHIFT"] = 75] = "INPLACE_LSHIFT";
+    OpCodeList[OpCodeList["INPLACE_RSHIFT"] = 76] = "INPLACE_RSHIFT";
+    OpCodeList[OpCodeList["INPLACE_AND"] = 77] = "INPLACE_AND";
+    OpCodeList[OpCodeList["INPLACE_XOR"] = 78] = "INPLACE_XOR";
+    OpCodeList[OpCodeList["INPLACE_OR"] = 79] = "INPLACE_OR";
+    OpCodeList[OpCodeList["BREAK_LOOP"] = 80] = "BREAK_LOOP";
+    OpCodeList[OpCodeList["WITH_CLEANUP"] = 81] = "WITH_CLEANUP";
+    OpCodeList[OpCodeList["LOAD_LOCALS"] = 82] = "LOAD_LOCALS";
+    OpCodeList[OpCodeList["RETURN_VALUE"] = 83] = "RETURN_VALUE";
+    OpCodeList[OpCodeList["IMPORT_STAR"] = 84] = "IMPORT_STAR";
+    OpCodeList[OpCodeList["EXEC_STMT"] = 85] = "EXEC_STMT";
+    OpCodeList[OpCodeList["YIELD_VALUE"] = 86] = "YIELD_VALUE";
+    OpCodeList[OpCodeList["POP_BLOCK"] = 87] = "POP_BLOCK";
+    OpCodeList[OpCodeList["END_FINALLY"] = 88] = "END_FINALLY";
+    OpCodeList[OpCodeList["BUILD_CLASS"] = 89] = "BUILD_CLASS";
+
     //Opcodes from here have an argument HAVE_ARGUMENT 90
-    STORE_NAME: 90,
-    DELETE_NAME: 91,
-    UNPACK_SEQUENCE: 92,
-    FOR_ITER: 93,
-    LIST_APPEND: 94,
-    STORE_ATTR: 95,
-    DELETE_ATTR: 96,
-    STORE_GLOBAL: 97,
-    DELETE_GLOBAL: 98,
-    DUP_TOPX: 99,
-    LOAD_CONST: 100,
-    LOAD_NAME: 101,
-    BUILD_TUPLE: 102,
-    BUILD_LIST: 103,
-    BUILD_SET: 104,
-    BUILD_MAP: 105,
-    LOAD_ATTR: 106,
-    COMPARE_OP: 107,
-    IMPORT_NAME: 108,
-    IMPORT_FROM: 109,
-    JUMP_FORWARD: 110,
-    JUMP_IF_FALSE_OR_POP: 111,
-    JUMP_IF_TRUE_OR_POP: 112,
-    JUMP_ABSOLUTE: 113,
-    POP_JUMP_IF_FALSE: 114,
-    POP_JUMP_IF_TRUE: 115,
-    LOAD_GLOBAL: 116,
-    CONTINUE_LOOP: 119,
-    SETUP_LOOP: 120,
-    SETUP_EXCEPT: 121,
-    SETUP_FINALLY: 122,
-    LOAD_FAST: 124,
-    STORE_FAST: 125,
-    DELETE_FAST: 126,
-    RAISE_VARARGS: 130,
+    OpCodeList[OpCodeList["STORE_NAME"] = 90] = "STORE_NAME";
+    OpCodeList[OpCodeList["DELETE_NAME"] = 91] = "DELETE_NAME";
+    OpCodeList[OpCodeList["UNPACK_SEQUENCE"] = 92] = "UNPACK_SEQUENCE";
+    OpCodeList[OpCodeList["FOR_ITER"] = 93] = "FOR_ITER";
+    OpCodeList[OpCodeList["LIST_APPEND"] = 94] = "LIST_APPEND";
+    OpCodeList[OpCodeList["STORE_ATTR"] = 95] = "STORE_ATTR";
+    OpCodeList[OpCodeList["DELETE_ATTR"] = 96] = "DELETE_ATTR";
+    OpCodeList[OpCodeList["STORE_GLOBAL"] = 97] = "STORE_GLOBAL";
+    OpCodeList[OpCodeList["DELETE_GLOBAL"] = 98] = "DELETE_GLOBAL";
+    OpCodeList[OpCodeList["DUP_TOPX"] = 99] = "DUP_TOPX";
+    OpCodeList[OpCodeList["LOAD_CONST"] = 100] = "LOAD_CONST";
+    OpCodeList[OpCodeList["LOAD_NAME"] = 101] = "LOAD_NAME";
+    OpCodeList[OpCodeList["BUILD_TUPLE"] = 102] = "BUILD_TUPLE";
+    OpCodeList[OpCodeList["BUILD_LIST"] = 103] = "BUILD_LIST";
+    OpCodeList[OpCodeList["BUILD_SET"] = 104] = "BUILD_SET";
+    OpCodeList[OpCodeList["BUILD_MAP"] = 105] = "BUILD_MAP";
+    OpCodeList[OpCodeList["LOAD_ATTR"] = 106] = "LOAD_ATTR";
+    OpCodeList[OpCodeList["COMPARE_OP"] = 107] = "COMPARE_OP";
+    OpCodeList[OpCodeList["IMPORT_NAME"] = 108] = "IMPORT_NAME";
+    OpCodeList[OpCodeList["IMPORT_FROM"] = 109] = "IMPORT_FROM";
+    OpCodeList[OpCodeList["JUMP_FORWARD"] = 110] = "JUMP_FORWARD";
+    OpCodeList[OpCodeList["JUMP_IF_FALSE_OR_POP"] = 111] = "JUMP_IF_FALSE_OR_POP";
+    OpCodeList[OpCodeList["JUMP_IF_TRUE_OR_POP"] = 112] = "JUMP_IF_TRUE_OR_POP";
+    OpCodeList[OpCodeList["JUMP_ABSOLUTE"] = 113] = "JUMP_ABSOLUTE";
+    OpCodeList[OpCodeList["POP_JUMP_IF_FALSE"] = 114] = "POP_JUMP_IF_FALSE";
+    OpCodeList[OpCodeList["POP_JUMP_IF_TRUE"] = 115] = "POP_JUMP_IF_TRUE";
+    OpCodeList[OpCodeList["LOAD_GLOBAL"] = 116] = "LOAD_GLOBAL";
+    OpCodeList[OpCodeList["CONTINUE_LOOP"] = 119] = "CONTINUE_LOOP";
+    OpCodeList[OpCodeList["SETUP_LOOP"] = 120] = "SETUP_LOOP";
+    OpCodeList[OpCodeList["SETUP_EXCEPT"] = 121] = "SETUP_EXCEPT";
+    OpCodeList[OpCodeList["SETUP_FINALLY"] = 122] = "SETUP_FINALLY";
+    OpCodeList[OpCodeList["LOAD_FAST"] = 124] = "LOAD_FAST";
+    OpCodeList[OpCodeList["STORE_FAST"] = 125] = "STORE_FAST";
+    OpCodeList[OpCodeList["DELETE_FAST"] = 126] = "DELETE_FAST";
+    OpCodeList[OpCodeList["RAISE_VARARGS"] = 130] = "RAISE_VARARGS";
+
     /* CALL_FUNCTION_XXX opcodes defined below depend on this definition */
-    CALL_FUNCTION: 131,
-    MAKE_FUNCTION: 132,
-    BUILD_SLICE: 133,
-    MAKE_CLOSURE: 134,
-    LOAD_CLOSURE: 135,
-    LOAD_DEREF: 136,
-    STORE_DEREF: 137,
+    OpCodeList[OpCodeList["CALL_FUNCTION"] = 131] = "CALL_FUNCTION";
+    OpCodeList[OpCodeList["MAKE_FUNCTION"] = 132] = "MAKE_FUNCTION";
+    OpCodeList[OpCodeList["BUILD_SLICE"] = 133] = "BUILD_SLICE";
+    OpCodeList[OpCodeList["MAKE_CLOSURE"] = 134] = "MAKE_CLOSURE";
+    OpCodeList[OpCodeList["LOAD_CLOSURE"] = 135] = "LOAD_CLOSURE";
+    OpCodeList[OpCodeList["LOAD_DEREF"] = 136] = "LOAD_DEREF";
+    OpCodeList[OpCodeList["STORE_DEREF"] = 137] = "STORE_DEREF";
+
     /* The next 3 opcodes must be contiguous and satisfy
     (CALL_FUNCTION_VAR - CALL_FUNCTION) & 3 == 1  */
-    CALL_FUNCTION_VAR: 140,
-    CALL_FUNCTION_KW: 141,
-    CALL_FUNCTION_VAR_KW: 142,
-    SETUP_WITH: 143,
+    OpCodeList[OpCodeList["CALL_FUNCTION_VAR"] = 140] = "CALL_FUNCTION_VAR";
+    OpCodeList[OpCodeList["CALL_FUNCTION_KW"] = 141] = "CALL_FUNCTION_KW";
+    OpCodeList[OpCodeList["CALL_FUNCTION_VAR_KW"] = 142] = "CALL_FUNCTION_VAR_KW";
+    OpCodeList[OpCodeList["SETUP_WITH"] = 143] = "SETUP_WITH";
+
     /* Support for opargs more than 16 bits long */
-    EXTENDED_ARG: 145,
-    SET_ADD: 146,
-    MAP_ADD: 147
-});
+    OpCodeList[OpCodeList["EXTENDED_ARG"] = 145] = "EXTENDED_ARG";
+    OpCodeList[OpCodeList["SET_ADD"] = 146] = "SET_ADD";
+    OpCodeList[OpCodeList["MAP_ADD"] = 147] = "MAP_ADD";
+})(OpCodeList || (OpCodeList = {}));
+;
 
 var fs = require('fs');
-var Enum = require('enum');
 fs.readFile(process.argv[2], function doneReading(err, bytecode) {
     if (err)
         throw err;
--- MasterFile.ts
+++ MasterFile.ts
@@ -270,6 +270,7 @@ function readCodeObject(bytecode:NodeBuffer, ptr:number, level:number) {
 }
 //Implemented bytecode functions in a OpCode class
 class OpCodeFunctions {
+
     public static STOP_CODE(){
         //do nothing
         console.log('STOP_CODE');
@@ -485,7 +486,9 @@ class OpCodeFunctions {
     public static STORE_GLOBAL(index:number){} 
     public static DELETE_GLOBAL(index:number){}
     public static DUP_TOPX(numItemsDup:number){} 
-    public static LOAD_CONST(index:number){}
+    public static LOAD_CONST(index:number){
+
+    }
     public static LOAD_NAME(index:number){}
     public static BUILD_TUPLE(numItems:number){} 
     public static BUILD_LIST(numItems:number){} 
@@ -578,20 +581,27 @@ function interpretBytecode(bytecode) {
     // http://daeken.com/2010-02-20_Python_Marshal_Format.html
     // http://nedbatchelder.com/blog/200804/the_structure_of_pyc_files.html
     parseBytecode(bytecode);
-
-    // console.log(byteObject);
-    // console.log(byteObject.code_object.code);
-    // console.log(byteObject.code_object.code[0]);
-    // console.log(byteObject.code_object.consts[0]);
     console.log(byteObject.interned_list);
-    // Execute Op Codes
+    //execute the initial opcodes in the code object
+    execBytecode();
+    
+  
+}
+//initalize the stack object
+var Stack = [];
+function execBytecode(){
+  // Execute Op Codes
+    for(var i=0; i < byteObject.code_object.code.length; i++){
+        var opcode = byteObject.code_object.code[i][0];
+        var operand = byteObject.code_object.code[i][1];
+        OpCodeFunctions[enums.OpCodeList[opcode](operand)];
+    }
+
+    return Stack.pop();
 }
 
 //initalize object to store information of various types 
 var byteObject:any = {};
-
-//initalize the stack object
-var Stack = [];
 
 var readByType = {};
 readByType['0'] = readNull;
@@ -619,137 +629,136 @@ readByType['c'] = readCodeObject;
 
 //https://android.googlesource.com/platform/prebuilts/python/darwin-x86/2.7.5/+/master/include/python2.7/opcode.h
 //enum list of all opcodes
-var OpCodeList = new Enum({
-    STOP_CODE:0, 
-    POP_TOP:1,
-    ROT_TWO:2,
-    ROT_THREE:3,
-    DUP_TOP:4,
-    ROT_FOUR:5,
-    NOP:9,
-    UNARY_POSITIVE:10,
-    UNARY_NEGATIVE:11,
-    UNARY_NOT:12,
-    UNARY_CONVERT:13,
-    UNARY_INVERT:15,
-    BINARY_POWER:19,
-    BINARY_MULTIPLY:20,
-    BINARY_DIVIDE:21,
-    BINARY_MODULO:22,
-    BINARY_ADD:23,
-    BINARY_SUBTRACT:24,
-    BINARY_SUBSCR:25,
-    BINARY_FLOOR_DIVIDE:26,
-    BINARY_TRUE_DIVIDE:27,
-    INPLACE_FLOOR_DIVIDE:28,
-    INPLACE_TRUE_DIVIDE:29,
-    SLICE:30,
-    //SLICE:31,
-    //SLICE:32,
-    //SLICE:33,
-    STORE_SLICE:40,
-    //STORE_SLICE:41,
-    //STORE_SLICE:42,
-   // STORE_SLICE:43,
-    DELETE_SLICE:50,
-   // DELETE_SLICE:51,
-    //DELETE_SLICE:52,
-    //DELETE_SLICE:53,
-    STORE_MAP:54,
-    INPLACE_ADD:55,
-    INPLACE_SUBTRACT:56,
-    INPLACE_MULTIPY:57,
-    INPLACE_DIVIDE:58,
-    INPLACE_MODULO:59,
-    STORE_SUBSCR:60,
-    DELETE_SUBSCR:61,
-    BINARY_LSHIFT:62,
-    BINARY_RSHIFT:63,
-    BINARY_AND:64,
-    BINARY_XOR:65,
-    BINARY_OR:66,
-    INPLACE_POWER:67,
-    GET_ITER:68,
-    PRINT_EXPR:70,
-    PRINT_ITEM:71,
-    PRINT_NEWLINE:72,
-    RINT_ITEM_TO:73,
-    PRINT_NEWLINE_TO:74,
-    INPLACE_LSHIFT:75,
-    INPLACE_RSHIFT:76,
-    INPLACE_AND:77,
-    INPLACE_XOR:78,
-    INPLACE_OR:79,
-    BREAK_LOOP:80,
-    WITH_CLEANUP:81,
-    LOAD_LOCALS:82,
-    RETURN_VALUE:83,
-    IMPORT_STAR:84,
-    EXEC_STMT:85,
-    YIELD_VALUE:86,
-    POP_BLOCK:87,
-    END_FINALLY:88,
-    BUILD_CLASS:89,
+enum OpCodeList {
+    STOP_CODE = 0,
+    POP_TOP = 1,
+    ROT_TWO = 2,
+    ROT_THREE = 3,
+    DUP_TOP = 4,
+    ROT_FOUR = 5,
+    NOP = 9,
+    UNARY_POSITIVE = 10,
+    UNARY_NEGATIVE = 11,
+    UNARY_NOT = 12,
+    UNARY_CONVERT = 13,
+    UNARY_INVERT = 15,
+    BINARY_POWER = 19,
+    BINARY_MULTIPLY = 20,
+    BINARY_DIVIDE = 21,
+    BINARY_MODULO = 22,
+    BINARY_ADD = 23,
+    BINARY_SUBTRACT = 24,
+    BINARY_SUBSCR = 25,
+    BINARY_FLOOR_DIVIDE = 26,
+    BINARY_TRUE_DIVIDE = 27,
+    INPLACE_FLOOR_DIVIDE = 28,
+    INPLACE_TRUE_DIVIDE = 29,
+    SLICE_0 = 30,
+    SLICE_1 = 31,
+    SLICE_2 = 32,
+    SLICE_3 = 33,
+    STORE_SLICE_0 = 40,
+    STORE_SLICE_1 = 41,
+    STORE_SLICE_2 = 42,
+    STORE_SLICE_3 = 43,
+    DELETE_SLICE_0 = 50,
+    DELETE_SLICE_1 = 51,
+    DELETE_SLICE_2 = 52,
+    DELETE_SLICE_3 = 53,
+    STORE_MAP = 54,
+    INPLACE_ADD = 55,
+    INPLACE_SUBTRACT = 56,
+    INPLACE_MULTIPY = 57,
+    INPLACE_DIVIDE = 58,
+    INPLACE_MODULO = 59,
+    STORE_SUBSCR = 60,
+    DELETE_SUBSCR = 61,
+    BINARY_LSHIFT = 62,
+    BINARY_RSHIFT = 63,
+    BINARY_AND = 64,
+    BINARY_XOR = 65,
+    BINARY_OR = 66,
+    INPLACE_POWER = 67,
+    GET_ITER = 68,
+    PRINT_EXPR = 70,
+    PRINT_ITEM = 71,
+    PRINT_NEWLINE = 72,
+    RINT_ITEM_TO = 73,
+    PRINT_NEWLINE_TO = 74,
+    INPLACE_LSHIFT = 75,
+    INPLACE_RSHIFT = 76,
+    INPLACE_AND = 77,
+    INPLACE_XOR = 78,
+    INPLACE_OR = 79,
+    BREAK_LOOP = 80,
+    WITH_CLEANUP = 81,
+    LOAD_LOCALS = 82,
+    RETURN_VALUE = 83,
+    IMPORT_STAR = 84,
+    EXEC_STMT = 85,
+    YIELD_VALUE = 86,
+    POP_BLOCK = 87,
+    END_FINALLY = 88,
+    BUILD_CLASS = 89,
     //Opcodes from here have an argument HAVE_ARGUMENT 90
-    STORE_NAME:90, //index in name list
-    DELETE_NAME:91, //index in name list
-    UNPACK_SEQUENCE:92, //number of sequence items
-    FOR_ITER:93,
-    LIST_APPEND:94,
-    STORE_ATTR:95, //index in name list
-    DELETE_ATTR:96, //index in name list
-    STORE_GLOBAL:97,//index in name list
-    DELETE_GLOBAL:98, //index in name list
-    DUP_TOPX:99, //number of items to duplicate
-    LOAD_CONST:100, //index in const list
-    LOAD_NAME:101, //index in name list
-    BUILD_TUPLE:102, //number of tuple items
-    BUILD_LIST:103, //number of list items
-    BUILD_SET:104, //number of set items
-    BUILD_MAP:105, //always 0 for now?
-    LOAD_ATTR:106, //index in name list
-    COMPARE_OP:107, //comparison operator
-    IMPORT_NAME:108, //index in name list
-    IMPORT_FROM:109, //index in name list
-    JUMP_FORWARD:110, //number of bytes to skip
-    JUMP_IF_FALSE_OR_POP:111, //target byte offset from beginning of code
-    JUMP_IF_TRUE_OR_POP:112, //target byte offset from beginning of code
-    JUMP_ABSOLUTE:113,  //target byte offset from beginning of code
-    POP_JUMP_IF_FALSE:114, //target byte offset from beginning of code
-    POP_JUMP_IF_TRUE:115, //target byte offset from beginning of code
-    LOAD_GLOBAL:116, //index in name list
-    CONTINUE_LOOP:119, //start of loop(absolute)
-    SETUP_LOOP:120, //target address(relative)
-    SETUP_EXCEPT:121, //target address(relative)
-    SETUP_FINALLY:122, //target address(relative)
-    LOAD_FAST:124, //local variable number
-    STORE_FAST:125,//local variable number
-    DELETE_FAST:126, //local variable number
-    RAISE_VARARGS:130, //number of raise arguments(1,2 or 3)
+    STORE_NAME = 90, //index in name list
+    DELETE_NAME = 91, //index in name list
+    UNPACK_SEQUENCE = 92, //number of sequence items
+    FOR_ITER = 93,
+    LIST_APPEND = 94,
+    STORE_ATTR = 95, //index in name list
+    DELETE_ATTR = 96, //index in name list
+    STORE_GLOBAL = 97,//index in name list
+    DELETE_GLOBAL = 98, //index in name list
+    DUP_TOPX = 99, //number of items to duplicate
+    LOAD_CONST = 100, //index in const list
+    LOAD_NAME = 101, //index in name list
+    BUILD_TUPLE = 102, //number of tuple items
+    BUILD_LIST = 103, //number of list items
+    BUILD_SET = 104, //number of set items
+    BUILD_MAP = 105, //always 0 for now?
+    LOAD_ATTR = 106, //index in name list
+    COMPARE_OP = 107, //comparison operator
+    IMPORT_NAME = 108, //index in name list
+    IMPORT_FROM = 109, //index in name list
+    JUMP_FORWARD = 110, //number of bytes to skip
+    JUMP_IF_FALSE_OR_POP = 111, //target byte offset from beginning of code
+    JUMP_IF_TRUE_OR_POP = 112, //target byte offset from beginning of code
+    JUMP_ABSOLUTE = 113,  //target byte offset from beginning of code
+    POP_JUMP_IF_FALSE = 114, //target byte offset from beginning of code
+    POP_JUMP_IF_TRUE = 115, //target byte offset from beginning of code
+    LOAD_GLOBAL = 116, //index in name list
+    CONTINUE_LOOP = 119, //start of loop(absolute)
+    SETUP_LOOP = 120, //target address(relative)
+    SETUP_EXCEPT = 121, //target address(relative)
+    SETUP_FINALLY = 122, //target address(relative)
+    LOAD_FAST = 124, //local variable number
+    STORE_FAST = 125,//local variable number
+    DELETE_FAST = 126, //local variable number
+    RAISE_VARARGS = 130, //number of raise arguments(1,2 or 3)
     /* CALL_FUNCTION_XXX opcodes defined below depend on this definition */
-    CALL_FUNCTION:131, //number of args + (number kwargs<<8)
-    MAKE_FUNCTION:132, //number defaults
-    BUILD_SLICE:133, //number of items
-    MAKE_CLOSURE:134,//number free vars
-    LOAD_CLOSURE:135, //load free variable from closure
-    LOAD_DEREF:136, //load and deference from closure cell
-    STORE_DEREF:137, //store into cell
+    CALL_FUNCTION = 131, //number of args + (number kwargs<<8)
+    MAKE_FUNCTION = 132, //number defaults
+    BUILD_SLICE = 133, //number of items
+    MAKE_CLOSURE = 134,//number free vars
+    LOAD_CLOSURE = 135, //load free variable from closure
+    LOAD_DEREF = 136, //load and deference from closure cell
+    STORE_DEREF = 137, //store into cell
     /* The next 3 opcodes must be contiguous and satisfy
        (CALL_FUNCTION_VAR - CALL_FUNCTION) & 3 == 1  */
-    CALL_FUNCTION_VAR:140, //number args + (number kwargs<<8)
-    CALL_FUNCTION_KW:141, //number args + (number kwargs<<8)
-    CALL_FUNCTION_VAR_KW:142, //number args + (number kwargs<<8)
-    SETUP_WITH:143,
+    CALL_FUNCTION_VAR = 140, //number args + (number kwargs<<8)
+    CALL_FUNCTION_KW = 141, //number args + (number kwargs<<8)
+    CALL_FUNCTION_VAR_KW = 142, //number args + (number kwargs<<8)
+    SETUP_WITH = 143,
     /* Support for opargs more than 16 bits long */
-    EXTENDED_ARG:145,
-    SET_ADD:146,
-    MAP_ADD:147
-});
+    EXTENDED_ARG = 145,
+    SET_ADD = 146,
+    MAP_ADD = 147
+};
 
 
 
 var fs = require('fs');
-var Enum = require('enum');
 fs.readFile(process.argv[2], function doneReading(err, bytecode) {
     if (err)
         throw err;
