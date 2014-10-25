9c6007e7193d8068d61cb1512f442535069d4f34
imgemp <imgemp@haar.cs.umass.edu>
2014-10-25 14:35:25 -0400

    Getting There...

--- divideTest.py
+++ divideTest.py
@@ -8,6 +8,9 @@ def divide(x, y):
     finally:
         print "executing finally clause"
 
+print('Normal Division ||||||||||||||||||||||||||||||||||||||||||||||||||')
 divide(2, 1)
+print('Divide by Zero Test ||||||||||||||||||||||||||||||||||||||||||||||')
 divide(2, 0)
+print('Divide with Strings Test||||||||||||||||||||||||||||||||||||||||||')
 divide("2", "1")
